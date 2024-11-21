c97dae57e4e9ac7191561d53542eb8f05e1aed5e
Geronti-L <gerontiliparteliani@gmail.com>
2024-11-21 12:00:11 -0500

    changed projectname

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -17,7 +17,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
         glue = "steps",
         plugin = {"pretty","html:target/report.html","json:target/report.json"},
         publish = true,
-        tags = "@Test3"
+        tags = "@TestAll"
 
 )
 public class TestRunner {
