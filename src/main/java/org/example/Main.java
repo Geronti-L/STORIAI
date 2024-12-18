4b40632c5f63288dbdecddd4f875b1e664508cc1
Geronti-L <gerontiliparteliani@gmail.com>
2024-12-18 17:48:13 -0500

    runner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,28 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import com.google.gson.Gson;
+
+import java.util.Scanner;
+
+class employee{
+    int id;
+    String name;
+    float sel;
+    Scanner sc=new Scanner(System.in);
+public employee(Scanner s){
+  this.sc=s;
+}
+public  void setEmpl(){
+    System.out.println("enter id:-");
+    id=sc.nextInt();
+    System.out.println("enter name:-");
+    name=sc.nextLine();
+}
+public void display(){
+    System.out.println("id="+id);
+    System.out.println("name"+name);
+}
+}
 class Person{
     String name;
     int age;
@@ -13,13 +35,17 @@ class Person{
     }
 }
 public class Main {
+
     public static void main(String[] args) {
+
         Gson gson=new Gson();
-        Person person=new Person("gero",40);
+        Person person=new Person("geronti",40);
         String json=gson.toJson(person);
         System.out.println(json);
         Person person1=gson.fromJson(json,Person.class);
         System.out.println(person1.name);
+//
+
     }
 
 
